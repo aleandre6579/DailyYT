15a0682ae3bf189f46b8b4c115f1c13d4a0e8e41
aleandre6579 <aleandre6579.simon@gmail.com>
2023-09-14 13:49:59 +0700

    Can store videos in mongodb

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
+from pymongo import MongoClient
 from pyyoutube import Api
 from pyyoutube import Client
 from traceback import print_exc
@@ -14,20 +15,47 @@ import json
 app = Flask(__name__)
 CORS(app)
 
+client = MongoClient('localhost', 27017)
+db = client.flask_db
+videos = db.videos
+
+# Constants
 TOKEN_FILE = "token.json"
+RECENT_IN_DAYS = 2
 
 authorizer = Authorizer(TOKEN_FILE, my_secrets.API_KEY, my_secrets.CLIENT_ID, my_secrets.CLIENT_SECRET)
 ACCESS_TOKEN = authorizer.token
 
 yt = YTStats(my_secrets.API_KEY, ACCESS_TOKEN)
 
-
 ## FLASK FUNCTIONS ##
 
-@app.get('/videos')
-def get_subs_videos():
-    #url = request.args.get('url', '')
+#myquery = { "videoId": "videoUrl" }
+#videos.delete_one(myquery)
 
+@app.get('/videos')
+def send_videos():
+    all_videos = get_all_db_videos()
+    if all_videos == []:
+        return "No videos"
+    print(all_videos)
+    return all_videos
+
+@app.get('/refresh')
+def refresh_videos():
+    new_videos = get_newest_videos()
+    for new_video in new_videos:
+        store_video(new_video)
+    return new_videos
+
+def get_all_db_videos():
+    return list(videos.find({}))
+
+def store_video(video):
+    videos.insert_one({'videoId': video[0], 'videoTitle': video[1], 'category': video[2], 'thumbnailUrl': video[3]})
+
+
+def get_newest_videos():
     subs_id = read_subs()
     all_videos_final = []
     for id in subs_id:
@@ -36,7 +64,7 @@ def get_subs_videos():
         video_ids = read_acts()
         i = 0
         for date in read_act_dates():
-            if not videoIsRecent(date, 2):
+            if not videoIsRecent(date, RECENT_IN_DAYS):
                 del video_ids[i]
                 i -= 1
             i += 1
@@ -55,8 +83,8 @@ def get_subs_videos():
             video_titles.append(video_title)
             video_thumbnails.append(video_thumbnail)
 
-        all_videos_final += list(zip(video_ids, video_title, category_titles, video_thumbnails))
-    return all_videos_final, 200
+        all_videos_final += list(zip(video_ids, video_titles, category_titles, video_thumbnails))
+    return all_videos_final
 
 
 if __name__ == "__main__":
--- frontend/main.js
+++ frontend/main.js
@@ -1,17 +1,37 @@
-const btn = document.getElementById("get_videos");
-btn.addEventListener("click", function () {
-    btn.disabled = true;
-    btn.innerHTML = "Fetching videos...";
+const video_btn = document.getElementById("get_videos");
+video_btn.addEventListener("click", function () {
+    video_btn.disabled = true;
+    video_btn.innerHTML = "Fetching videos...";
 
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://127.0.0.1:5000/videos?url=" + "THISISURL", true);
+    xhr.open("GET", "http://127.0.0.1:5000/videos", true);
     xhr.onload = function () {
         var text = xhr.responseText;
-        const p = document.getElementById("output");
+        const p = document.getElementById("output_videos");
         p.innerHTML = text;
-        btn.disabled = false;
-        btn.innerHTML = "Get videos";
+        video_btn.disabled = false;
+        video_btn.innerHTML = "Get videos";
     }
     xhr.send();
 });
 
+const refresh_btn = document.getElementById("refresh");
+refresh_btn.addEventListener("click", function () {
+    refresh_btn.disabled = true;
+    refresh_btn.innerHTML = "Refreshing...";
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://127.0.0.1:5000/refresh", true);
+    xhr.onload = function () {
+        var text = xhr.responseText;
+        const p = document.getElementById("output_videos");
+        p.innerHTML = text;
+        refresh_btn.disabled = false;
+        refresh_btn.innerHTML = "Refresh";
+    }
+    xhr.send();
+});
+
+document.addEventListener('alpine:init', () => {
+    Alpine.store('activeCategory', 'All')
+})
